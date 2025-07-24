13b7aaf371672f7efd713ae5275be8a398443396
AlysonEast <a.east.ecology@gmail.com>
2025-07-24 13:41:19 -0400

    breaking up process to create testing data into two steps. First step take in annotatons and crops images, second step reprocesses bounding boxes. Between steps I look at the annotations with the cropped images and add missind trees created by the buffering of the image boudary

---
+++ developTestingData_step2.R
@@ -0,0 +1,71 @@
+#### 1. Load Required Libraries ####
+library(terra)
+library(sf)
+library(sp)
+
+setwd("/media/aly/Penobscot/ForestScaling/Crown_Segmentation/LiDAR")
+
+#### Format bounding boxes that are manually annotated in QGIS ####
+## Here we read those annotations back in to process and clean data for model Testing
+# Add manually annotated boxes
+bboxlist<-list.files("../Imagery/NAIP/Testing/bbox", pattern = "*.shp")
+bboxlist
+
+#Process the first entry and use it to make plots
+NAIP_bbox<-read_sf(paste0("../Imagery/NAIP/Testing/bbox/",bboxlist[1]))
+buffered_box <- st_bbox(NAIP_bbox)
+bbox_crop <- extent(buffered_box)
+
+# Cleaning and generating DeepForest Annotations for Testing
+bbox_folder <- "../Imagery/NAIP/Testing/bbox/"
+annotations_csv <- "../Imagery/NAIP/Testing/Crop_Images/annotations.csv"
+
+annotations <- data.frame(
+  image_path = character(),
+  xmin       = integer(),
+  ymin       = integer(),
+  xmax       = integer(),
+  ymax       = integer(),
+  label      = character(),  # e.g., "Tree"
+  stringsAsFactors = FALSE
+)
+
+shapefiles <- list.files(bbox_folder, pattern = "\\.shp$", full.names = TRUE)
+bboxlist <- list.files(bbox_folder, pattern = "\\.shp$", full.names = FALSE)
+
+for (j in seq_along(shapefiles)) {
+  sa <- st_read(shapefiles[j], quiet = TRUE)
+  bb <- st_bbox(sa)
+  cropped <- NULL
+  
+  # Pixel conversion, annotation block stays the same...
+  res_xy <- res(cropped)
+  ext_xy <- extent(cropped)
+  for (i in seq_len(nrow(sa))) {
+    feature <- sa[i, ]
+    bb_f <- st_bbox(feature)
+    xmin_px <- round((bb_f["xmin"] - ext_xy@xmin) / res_xy[1])
+    xmax_px <- round((bb_f["xmax"] - ext_xy@xmin) / res_xy[1])
+    ymin_px <- round((ext_xy@ymax - bb_f["ymax"]) / res_xy[2])
+    ymax_px <- round((ext_xy@ymax - bb_f["ymin"]) / res_xy[2])
+    
+    annotations <- rbind(
+      annotations,
+      data.frame(
+        image_path = basename(out_name),
+        xmin       = xmin_px,
+        ymin       = ymin_px,
+        xmax       = xmax_px,
+        ymax       = ymax_px,
+        label      = "Tree",
+        stringsAsFactors = FALSE
+      )
+    )
+  }
+}
+
+table(annotations$image_path)
+head(annotations)
+annotations$label<-"Tree"
+annotations<-na.omit(annotations)
+write.csv(annotations, annotations_csv, row.names = FALSE, quote = FALSE)
